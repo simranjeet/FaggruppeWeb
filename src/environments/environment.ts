302ed63a091619c35d79c8b6ca97c14a28bb824b
Simranjeet <simranjeetdeol164@hotmail.com>
2018-10-02 20:32:06 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   config: {
-    tenant: 'simranjeetdeol164@hotmail.com.onmicrosoft.com',
+    tenant: 'simranjeetdeol164hotmail.onmicrosoft.com',
     clientId: '5c52f36a-6ac9-48cc-aaeb-c10b9ce5d92e',
     redirectUri: window.location.origin + '/',
     postLogoutRedirectUri: window.location.origin + '/'

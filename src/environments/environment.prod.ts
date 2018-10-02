ce5371c755a3cd4869c7ea5178c61b1f3d01afca
Simranjeet <simranjeetdeol164@hotmail.com>
2018-10-02 19:43:56 +0200

    Added environment config and adal library

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    tenant: 'simranjeetdeol164@hotmail.com.onmicrosoft.com',
+    clientId: '5c52f36a-6ac9-48cc-aaeb-c10b9ce5d92e',
+    redirectUri: window.location.origin + '/',
+    postLogoutRedirectUri: window.location.origin + '/'
+  },
+  api: {
+    clientId: 'b83dcd90-4621-4da3-ab76-362282613bad',
+    apiUrl: 'https://faggruppewebapp.azurewebsites.net/api/cosmos'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    tenant: 'simranjeetdeol164@hotmail.com.onmicrosoft.com',
+    clientId: '5c52f36a-6ac9-48cc-aaeb-c10b9ce5d92e',
+    redirectUri: window.location.origin + '/',
+    postLogoutRedirectUri: window.location.origin + '/'
+  },
+  api: {
+    clientId: 'b83dcd90-4621-4da3-ab76-362282613bad',
+    apiUrl: 'https://faggruppewebapp.azurewebsites.net/api/cosmos'
+  }
 };
 
 /*
